1b3d3c31ef8c70f7cc2f0bffa6e1eb372a80a1eb
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2025-03-14 19:26:32 +0600

    setup layout

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+Route::get('/users', [DashboardController::class, 'dashboard'])->name('users');
+Route::get('/roles', [DashboardController::class, 'dashboard'])->name('roles');
