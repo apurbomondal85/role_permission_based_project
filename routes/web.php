e155f8dcc22e99b9c3eba03d490ab5134a8afbaa
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2025-03-14 17:58:51 +0600

    login is done

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
